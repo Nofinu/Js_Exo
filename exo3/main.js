df780d737c2b3e6c48a16257b2e581b9e0c9b4e1
Léo Lhomme <leolhommeo@gmail.com>
2023-01-10 12:19:37 +0100

    correction exo 3

--- exo3/main.js
+++ exo3/main.js
@@ -1,9 +1,9 @@
-let diametre =0,perimetre = 0 , air=0 , rayon = 0, message="";
+let diametre,perimetr, air, message;
 
 diametre = Number(prompt("entrer la valeur du diametre du cercle :"));
 rayon = diametre/2;
 perimetre = diametre * Math.PI;
-air= Math.PI*Math.pow(rayon, 2);
+air= Math.PI*Math.pow(diametre/2, 2);
 
 message=`Le perimetre de notre cercle de diametre = ${diametre} vaut : ${Math.round(perimetre)}`
 
