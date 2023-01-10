00922cd3aacc3864fc1463a0018fd881adf6d718
Léo Lhomme <leolhommeo@gmail.com>
2023-01-10 12:02:52 +0100

    exo3

---
+++ exo3/main.js
@@ -0,0 +1,15 @@
+let diametre =0,perimetre = 0 , air=0 , rayon = 0, message="";
+
+diametre = Number(prompt("entrer la valeur du diametre du cercle :"));
+rayon = diametre/2;
+perimetre = diametre * Math.PI;
+air= Math.PI*Math.pow(rayon, 2);
+
+message=`Le perimetre de notre cercle de diametre = ${diametre} vaut : ${perimetre}`
+
+console.log(message)
+
+message = `L'air de notre cercle de diametre = ${diametre} vaut : ${air}`
+
+console.log(message)
+
