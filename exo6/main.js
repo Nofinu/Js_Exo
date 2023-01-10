557c49a22974fa4dcad8ce0872a5f364dbb5c969
Léo Lhomme <leolhommeo@gmail.com>
2023-01-10 14:16:29 +0100

    ajout exo6

---
+++ exo6/main.js
@@ -0,0 +1,16 @@
+// initialisation des variable
+let tva,prix,valeurTva,prixTtc,affichage;
+
+//entrer des valeur
+
+prix = Number(prompt("quel est le prix de votre article :"));
+tva = Number(prompt("quel est le % tva appliqué (valeur entre 0 et 100):"));
+
+//calcul
+
+valeurTva = prix * (tva/100);
+prixTtc = prix + valeurTva;
+
+//affichage
+affichage = `pour un article de ${prix}€ et une TVA de ${tva}% on a donc une valeur de TVA de ${valeurTva} et donc un article en TTC a ${prixTtc}€`
+alert(affichage)
