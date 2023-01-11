2f546aa16bf24ba50f9e06fd58f72d3ddf86f098
Léo Lhomme <leolhommeo@gmail.com>
2023-01-11 11:09:32 +0100

    modif exo10

--- exo10/main.js
+++ exo10/main.js
@@ -4,21 +4,23 @@ let entry="",message="";
 //entrer des valeurs
 entry = (prompt("Quelle boisson voullez vous (entrer la valeur corespondante) : \n1.Eau\n2.Jus d'oranges\n3.Limonades\n4.Café\n5.Thé"));
 
+message = "Vous avez choisi"
+
 switch(entry){
     case "1":
-        message = "Vous avez choisi de l'eau.";
+        message += " de l'eau.";
         break;
     case "2":
-        message = "Vous avez choisi du jus d'oranges.";
+        message += " d'oranges.";
         break;
     case "3":
-        message = "Vous avez choisi de la limonade.";
+        message += " de la limonade.";
         break;
     case "3":
-        message = "Vous avez choisi du café.";
+        message += " du café.";
         break;
     case "5":
-        message = "Vous avez choisi de thé.";
+        message += " du thé.";
         break;
     default:
         message = "valeurs incorecte elle ne corespond pas a un de nos produits";
