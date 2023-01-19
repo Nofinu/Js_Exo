9e5967c4ef0b1ea59f9f955b29919f8b07863c7b
Léo Lhomme <leolhommeo@gmail.com>
2023-01-19 12:15:45 +0100

    ajout exo2 semaine2

--- Exercices_semaine_2/exo2/main.js
+++ Exercices_semaine_2/exo2/main.js
@@ -1,1 +1,31 @@
 //intialisation
+let tabChien = [{nom:"max",race:"unerace",age:5},{nom:"roukie",race:"uneautrerace",age:8}],compteurChien=2;
+const mySend = document.querySelector('#send');
+const nomChien = document.querySelector('#nomChien');
+const raceChien = document.querySelector('#raceChien');
+const ageChien = document.querySelector('#ageChien');
+const mySelect = document.querySelector('#my-select');
+const affichage = document.querySelector('#affichage');
+
+function addChien(newNom,newRace,newAge){
+    tabChien.push({nom:newNom,race:newRace,age:newAge});
+}
+
+mySend.addEventListener('click',() =>{
+    let nom = nomChien.value,race = raceChien.value,age = ageChien.value;
+    addChien(nom,race,age);
+    mySelect.innerHTML += `<option value="${compteurChien}">${nom}</option>`
+    console.table(tabChien);
+    compteurChien++;
+});
+
+mySelect.addEventListener("change", () => {
+    if(mySelect.value != ""){
+    affichage.textContent = `vous avez selectionné ${tabChien[mySelect.value].nom} \nde la race ${tabChien[mySelect.value].race}\net qui a ${tabChien[mySelect.value].age} ans.`;
+
+    }
+    else{
+        affichage.textContent = "";
+    }
+});
+
