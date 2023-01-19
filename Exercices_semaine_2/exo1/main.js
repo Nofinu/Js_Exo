206b6d81aa210fb616051bfd13035affe06719dc
Léo Lhomme <leolhommeo@gmail.com>
2023-01-19 09:35:41 +0100

    ajout exo1  semaine 2

---
+++ Exercices_semaine_2/exo1/main.js
@@ -0,0 +1,58 @@
+//intialisation
+let noteMax=0,noteMin=20,entry=0,tabNote=[],compteur=0;
+
+const mySaisie = document.querySelector('#saisieBtn');
+const myBestNote = document.querySelector('#bestNoteBtn');
+const myWorstNote = document.querySelector('#worstNoteBtn');
+const myMean = document.querySelector('#meanBtn');
+const myInputArea = document.querySelector('#inputArea');
+const myEntryText = document.querySelector('#entryText');
+const myEntryBtn = document.querySelector('#entryBtn');
+const myResultWorstNote = document.querySelector('#resultWorstNote');
+const myResultBestNote = document.querySelector('#resultBestNote');
+const myResultMean = document.querySelector('#resultMean');
+const myListeNote = document.querySelector('#listeNote')
+
+function calcmean (liste){
+    let somme =0;
+    for(let i in liste){
+        somme += liste[i];
+        console.log(liste[i])
+    }
+    return Math.round(somme/liste.length);
+}
+
+
+mySaisie.addEventListener('click',() =>{
+    myInputArea.classList.remove("hidden");
+});
+document.addEventListener("keypress",(e) =>{
+    if(e.key === "Escape"){
+        myInputArea.classList.add("hidden");
+    }
+});
+
+myEntryBtn.addEventListener('click',() =>{
+    let entry = 0;
+    entry = Number(myEntryText.value);
+    if (entry < noteMin){
+        noteMin = entry;
+    }
+    if(entry > noteMax){
+        noteMax = entry;
+    }
+    tabNote[compteur] = entry;
+    compteur++;
+    myListeNote.innerHTML += `<li>en note ${compteur}, vous avez saisi ${entry}/20.</li>`
+});
+
+myWorstNote.addEventListener('click',() =>{
+    myResultWorstNote.textContent = `la pire note est : ${noteMin}`;
+});
+myBestNote.addEventListener('click',() =>{
+    myResultBestNote.textContent = `la meilleure note est : ${noteMax}`;
+});
+myMean.addEventListener('click',() =>{
+    myResultMean.textContent =`la moyenne est de : ${calcmean(tabNote)}`;
+});
+
