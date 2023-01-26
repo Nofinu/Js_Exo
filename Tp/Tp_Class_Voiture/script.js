b2a018d24f320d603a32df0eae0b8ca8ac1dad7f
Léo Lhomme <leolhommeo@gmail.com>
2023-01-26 14:26:12 +0100

    tp class voiture

--- Tp/Tp_Class_Voiture/script.js
+++ Tp/Tp_Class_Voiture/script.js
@@ -3,7 +3,6 @@ import {Car} from "./Class/class.js";
 const voiture1 = new Car("Bmw","Serie 1",80);
 const voiture2 = new Car("Mercedes","GLB",100);
 const liste = document.querySelector('ul');
-console.log(liste)
 
 function affichage (){
     liste.innerHTML +=`<li>la ${voiture1.marque}, ${voiture1.modele} roule a <b>${voiture1.vitesse} km/h.</b></li>`
@@ -21,4 +20,4 @@ voiture1.accelerer();
 voiture2.tourner();
 affichage();
 voiture2.tourner();
-affichage
+affichage();
