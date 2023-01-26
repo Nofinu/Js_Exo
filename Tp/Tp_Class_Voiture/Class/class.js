e64daeb384e5ae76a92bfbaf19a6ca30acfe84bb
Léo Lhomme <leolhommeo@gmail.com>
2023-01-26 14:39:10 +0100

    tp class voiture

--- Tp/Tp_Class_Voiture/Class/class.js
+++ Tp/Tp_Class_Voiture/Class/class.js
@@ -6,8 +6,10 @@ export class Car {
     }
     accelerer(){
         this.vitesse += 10;
+        return `<li>la ${this.marque}, ${this.modele} roule a <b>${this.vitesse} km/h.</b></li>`
     }
     tourner(){
         this.vitesse -=5;
+        return`<li>la ${this.marque}, ${this.modele} roule a <b>${this.vitesse} km/h.</b></li>`
     }
 }
--- Tp/Tp_Class_Voiture/script.js
+++ Tp/Tp_Class_Voiture/script.js
@@ -4,20 +4,10 @@ const voiture1 = new Car("Bmw","Serie 1",80);
 const voiture2 = new Car("Mercedes","GLB",100);
 const liste = document.querySelector('ul');
 
-function affichage (){
-    liste.innerHTML +=`<li>la ${voiture1.marque}, ${voiture1.modele} roule a <b>${voiture1.vitesse} km/h.</b></li>`
-    liste.innerHTML +=`<li>la ${voiture2.marque}, ${voiture2.modele} roule a <b>${voiture2.vitesse} km/h.</b></li>`
-}
-
-
-voiture1.accelerer();
-voiture2.accelerer();
-affichage();
-voiture1.accelerer();
-voiture2.accelerer();
-affichage();
-voiture1.accelerer();
-voiture2.tourner();
-affichage();
-voiture2.tourner();
-affichage();
+liste.innerHTML += voiture1.accelerer();
+liste.innerHTML += voiture2.accelerer();
+liste.innerHTML += voiture1.accelerer();
+liste.innerHTML += voiture2.accelerer();
+liste.innerHTML += voiture1.accelerer();
+liste.innerHTML += voiture2.tourner();
+liste.innerHTML += voiture2.tourner();
