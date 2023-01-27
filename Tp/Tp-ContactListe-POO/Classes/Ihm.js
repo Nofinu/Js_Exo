4f9f91672d9400e3d2f39a4ea8077b65271f322e
Leo Lhomme <leolhommeo@gmail.com>
2023-01-27 11:09:42 +0100

    ajout d'une Ihm

---
+++ Tp/Tp-ContactListe-POO/Classes/Ihm.js
@@ -0,0 +1,37 @@
+import Contact from "./Contact.js";
+
+export default class Ihm {
+    constructor(form,inputs,table){
+        this.form = form;
+        this.inputs = inputs;
+        this.table =table;
+        this.contacts =[];
+
+        this.form.addEventListener('submit',(e)=>{
+            e.preventDefault();
+            if(inputs[0].checked){
+                console.log()
+                const contact = new Contact(this.inputs[0].value,this.inputs[2].value,this.inputs[3].value,this.inputs[4].value,this.inputs[5].value,this.inputs[6].value);
+                this.contacts.push(contact);
+            }
+            else if (inputs[1].checked){
+                const contact = new Contact(this.inputs[1].value,this.inputs[2].value,this.inputs[3].value,this.inputs[4].value,this.inputs[5].value,this.inputs[6].value);
+                this.contacts.push(contact);
+            }
+            this.refreshTab();
+        });
+    }
+    refreshTab (){
+        this.table.innerHTML ="";
+        for(let contact of this.contacts){
+            this.table.innerHTML += `<tr>
+            <td>${contact.titre}</td>
+            <td>${contact.nom}</td>
+            <td>${contact.prenom}</td>
+            <td>${contact.dateNaissance}</td>
+            <td>${contact.telephone}</td>
+            <td>${contact.email}</td>
+            </tr>`
+        }
+    }
+}
--- Tp/Tp-ContactListe-POO/index.js
+++ Tp/Tp-ContactListe-POO/index.js
@@ -1,33 +1,7 @@
-import Contact from "./Classes/Contact.js";
+import Ihm from "./Classes/Ihm.js";
 
-let contacts = [];
-const envoyer = document.getElementById('envoyer');
+const form = document.getElementById('formContainer');
 const inputs = document.querySelectorAll('input');
-const table = document.querySelector('table');
+const table = document.getElementById('tableBody');
 
-function refreshTab (){
-    for(let contact of contacts){
-        table.innerHTML += `<tr>
-        <td>${contact.titre}</td>
-        <td>${contact.nom}</td>
-        <td>${contact.prenom}</td>
-        <td>${contact.dateNaissance}</td>
-        <td>${contact.telephone}</td>
-        <td>${contact.email}</td>
-        </tr>`
-    }
-}
-
-
-envoyer.addEventListener('click',()=>{
-    if(inputs[0].checked){
-        console.log()
-        const contact = new Contact(inputs[0].value,inputs[2].value,inputs[3].value,inputs[4].value,inputs[5].value,inputs[6].value);
-        contacts.push(contact);
-    }
-    else if (inputs[1].checked){
-        const contact = new Contact(inputs[1].value,inputs[2].value,inputs[3].value,inputs[4].value,inputs[5].value,inputs[6].value);
-        contacts.push(contact);
-    }
-    refreshTab()
-});
+let ihm = new Ihm(form,inputs,table);
